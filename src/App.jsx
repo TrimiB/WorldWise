2fb3c2c778ce6a7db9a88068e4b07039aea1d077
Kushtrim <bujupikushtrim@outlook.com>
2024-05-21 22:48:11 +0200

    implementing routes for all components in pages folder.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product.jsx';
+import Pricing from './pages/Pricing.jsx';
+import Home from './pages/Home.jsx';
+import PageNotFound from './pages/PageNotFound.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='product' element={<Product />} />
+        <Route path='pricing' element={<Pricing />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
