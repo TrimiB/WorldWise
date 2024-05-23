772a9ddedfefacd29e5aaecda7da1e80cd4e33ef
Kushtrim <bujupikushtrim@outlook.com>
2024-05-23 13:16:37 +0200

    Importing some prebuild components and starting intigraton

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product.jsx';
 import Pricing from './pages/Pricing.jsx';
-import Home from './pages/Home.jsx';
+import Homepage from './pages/Homepage.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Homepage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
         <Route path='*' element={<PageNotFound />} />
