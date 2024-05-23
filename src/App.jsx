06e69b5936d7245652d65277f051c61edce33d86
Kushtrim <bujupikushtrim@outlook.com>
2024-05-23 16:03:55 +0200

    created CityList component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from './pages/Homepage.jsx';
 import Login from './pages/Login.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
 import AppLayout from './pages/AppLayout.jsx';
+import CityList from './components/CityList.jsx';
 
 function App() {
   return (
@@ -15,8 +16,8 @@ function App() {
         <Route path='/pricing' element={<Pricing />} />
         <Route path='/login' element={<Login />} />
         <Route path='/app' element={<AppLayout />}>
-          <Route index element={<p>List of Cities</p>} />
-          <Route path='cities' element={<p>List of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path='cities' element={<CityList />} />
           <Route path='countries' element={<p>Countries</p>} />
           <Route path='form' element={<p>Form</p>} />
         </Route>
