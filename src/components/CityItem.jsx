5e9a75bfd4d9c98c7d6fbf42092c8a2c2e7b4cf4
Kushtrim <bujupikushtrim@outlook.com>
2024-05-23 21:54:23 +0200

    remove cl

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -8,8 +8,8 @@ const formatDate = (date) =>
   }).format(new Date(date));
 
 function CityItem({ city }) {
-  console.log(city);
   const { cityName, emoji, date } = city;
+
   return (
     <li className={styles.cityItem}>
       <span className={styles.emoji}>{emoji}</span>
