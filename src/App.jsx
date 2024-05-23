ee7cc7b1371efdd11b2b2ecf4731710a7ab99948
Kushtrim <bujupikushtrim@outlook.com>
2024-05-23 16:16:36 +0200

    importing and using useState and useEffect. Padding in cities and isLoading to CityList

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 import Product from './pages/Product.jsx';
 import Pricing from './pages/Pricing.jsx';
 import Homepage from './pages/Homepage.jsx';
@@ -7,7 +9,28 @@ import PageNotFound from './pages/PageNotFound.jsx';
 import AppLayout from './pages/AppLayout.jsx';
 import CityList from './components/CityList.jsx';
 
+const BASE_URL = 'http://localhost:8000';
+
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -16,8 +39,8 @@ function App() {
         <Route path='/pricing' element={<Pricing />} />
         <Route path='/login' element={<Login />} />
         <Route path='/app' element={<AppLayout />}>
-          <Route index element={<CityList />} />
-          <Route path='cities' element={<CityList />} />
+          <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+          <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />} />
           <Route path='countries' element={<p>Countries</p>} />
           <Route path='form' element={<p>Form</p>} />
         </Route>
