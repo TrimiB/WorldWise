b2bf1920c17926601f722d20e22f32c500f8136f
Kushtrim <bujupikushtrim@outlook.com>
2024-05-23 13:47:38 +0200

    Fixing the navigation

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Product from './pages/Product.jsx';
 import Pricing from './pages/Pricing.jsx';
 import Homepage from './pages/Homepage.jsx';
+import Login from './pages/Login.jsx';
 import PageNotFound from './pages/PageNotFound.jsx';
+import AppLayout from './pages/AppLayout.jsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='product' element={<Product />} />
-        <Route path='pricing' element={<Pricing />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/pricing' element={<Pricing />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/app' element={<AppLayout />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
