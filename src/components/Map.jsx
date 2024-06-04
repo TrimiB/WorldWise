0bcc2e02c3069be2d72a8f57606a2132a8013660
Kushtrim <bujupikushtrim@outlook.com>
2024-06-04 21:39:02 +0200

    using useUrlPosition hook

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,19 +5,17 @@ import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesProvider';
 import { useGeolocation } from '../hooks/useGeolocation';
 import Button from './Button';
+import { useUrlPosition } from '../hooks/useUrlPosition';
 
 function Map() {
   const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([37.0902, -95.7129]);
-  const [searchParams] = useSearchParams();
   const {
     isLoading: isLoadingPosition,
     position: geolocationPosition,
     getPosition,
   } = useGeolocation();
-
-  const mapLat = searchParams.get('lat');
-  const mapLng = searchParams.get('lng');
+  const [mapLat, mapLng] = useUrlPosition();
 
   useEffect(() => {
     if (mapLat && mapLng) {
